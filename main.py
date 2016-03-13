4697532e13fcba0f5c1751a2caf990088e444664
geniuszhaoyi <genius.zhaoyi@gmail.com>
2016-03-14 01:56:46 +0800

    asd

--- main.py
+++ main.py
@@ -15,12 +15,22 @@ root.title("HelloWorld")
 root.geometry("800x600")
 root.resizable(width=False, height=False)
 
-canvas=Canvas(root,width=800,height=600,bg='white')
-#canvas.pack()
+canvas=Canvas(root,width=770,height=588,bg='white') # 35*28
+canvas.pack()
 
-label=Label(root,text='0')
+f=open('farm.map')
+ls=f.readlines()
+f.close()
+
+for i in range(22):
+	for j in range(21):
+		if ls[j][i]=='0':
+			canvas.create_rectangle(i*35+0,j*28+0,i*35+34,j*28+27,fill='grey',outline='grey')
+
+'''label=Label(root,text='0')
 label.pack()
 timer()
+'''
 
 canvas.create_rectangle(10,10,50,50,fill='blue')
 
